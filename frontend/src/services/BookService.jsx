135be0295c5b462ba102ed1223d11b12185142ae
Felipe <felipeclave@gmail.com>
2025-04-18 20:08:03 -0300

    Alteração no serviço de livros do axios

--- frontend/src/services/BookService.jsx
+++ frontend/src/services/BookService.jsx
@@ -3,22 +3,22 @@ import axios from "axios";
 const booksAPI = axios.create({baseURL: "https://my-bookshelf-uobo.onrender.com"})
 
 async function getBooks( search = "") {
-    const response = await booksAPI.get("/", { params: { search } });
+    const response = await booksAPI.get("/books", { params: { search } });
     return response.data;
 }
 
 async function postBook(newBook) {
-    const response = await booksAPI.post("/", newBook);
+    const response = await booksAPI.post("/books", newBook);
     return response.data;
 }
 
 async function patchBook(id, updatedBook) {
-    const response = await booksAPI.patch(`/${id}`, updatedBook);
+    const response = await booksAPI.patch(`/books/${id}`, updatedBook);
     return response.data;
 }
 
 async function deleteBook(id) {
-    const response = await booksAPI.delete(`/${id}`);
+    const response = await booksAPI.delete(`/books/${id}`);
     return response.data;
 }
 
